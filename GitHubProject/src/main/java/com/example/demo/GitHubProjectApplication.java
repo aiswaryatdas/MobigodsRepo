15322b42ae4d2cc059c66f2a2281d80aa3da3939
hp <hp@192.168.1.4>
2019-12-05 12:41:07 +0530

    new commit by aishu

---
+++ GitHubProject/src/main/java/com/example/demo/GitHubProjectApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitHubProjectApplication.class, args);
+		System.out.println("Hai....!!");
+	}
+
+}
